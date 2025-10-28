25b4abae4bcbcd2d8036ba83d86c1c99801e5b3b
Josii13 <caleb2002josias>
2025-10-28 19:26:14 +0100

    configuration bd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,27 +14,47 @@ class DatabaseSeeder extends Seeder
         // Créer un utilisateur admin
         User::create([
             'name' => 'Admin',
-            'email' => 'admin@whatsapp.com',
+            'email' => 'admin@yeswecange.com',
             'password' => Hash::make('password123'),
             'email_verified_at' => now(),
         ]);
 
-        // Créer des contacts de test
-        $campagnes = [
-            'Campagne_Printemps',
-            'Campagne_Ete',
-            'Campagne_Promo',
-            'Campagne_Black_Friday',
-            'Campagne_Noel'
-        ];
+        User::create([
+            'name' => 'Admin',
+            'email' => 'josias.ci@yeswecange.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
 
-        for ($i = 1; $i <= 50; $i++) {
-            MessageWhatsapp::create([
-                'numero_telephone' => '+336' . str_pad(rand(10000000, 99999999), 8, '0', STR_PAD_LEFT),
-                'nom_campagne' => $campagnes[array_rand($campagnes)],
-                'date_envoi' => now()->subDays(rand(0, 30)),
-                'id_twilio' => 'TW' . strtoupper(bin2hex(random_bytes(8))),
-            ]);
-        }
+        User::create([
+            'name' => 'Manager',
+            'email' => 'hermann.koffi@yeswecange.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+        User::create([
+            'name' => 'Manager',
+            'email' => 'julien@yeswecange.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+
+        // // Créer des contacts de test
+        // $campagnes = [
+        //     'Campagne_Printemps',
+        //     'Campagne_Ete',
+        //     'Campagne_Promo',
+        //     'Campagne_Black_Friday',
+        //     'Campagne_Noel'
+        // ];
+
+        // for ($i = 1; $i <= 50; $i++) {
+        //     MessageWhatsapp::create([
+        //         'numero_telephone' => '+336' . str_pad(rand(10000000, 99999999), 8, '0', STR_PAD_LEFT),
+        //         'nom_campagne' => $campagnes[array_rand($campagnes)],
+        //         'date_envoi' => now()->subDays(rand(0, 30)),
+        //         'id_twilio' => 'TW' . strtoupper(bin2hex(random_bytes(8))),
+        //     ]);
+        // }
     }
 }
